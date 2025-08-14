b0cdb80b9b2aa09502207518bfa4690870b5041c
marcuszermaaa <142676492+marcuszermaaa@users.noreply.github.com>
2025-08-14 12:18:07 -0300

    v0.02
    v0.02

--- js/journal.js
+++
@@ -1,7 +0,0 @@
-// js/journal.js - PONTO DE ENTRADA (NÃO MUDA)
-
-import { JournalManager } from './managers/journalManager.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    new JournalManager();
-});
